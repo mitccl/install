53f250c86e1f9a988bd5d5e66a2adfad9ca89088
metalmagic767 <43501347+metalmagic767@users.noreply.github.com>
2019-03-10 23:24:04 +0000

    commit

---
+++ goldengun/zips/build/addons/plugin.video.jor-el/service.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Jor-El Add-on
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from resources.lib.modules import log_utils
+from resources.lib.modules import control
+
+control.execute('RunPlugin(plugin://%s)' % control.get_plugin_url({'action': 'service'}))
+
+try:
+    ModuleVersion = control.addon('script.module.jor-el').getAddonInfo('version')
+    AddonVersion = control.addon('plugin.video.jor-el').getAddonInfo('version')
+    RepoVersion = control.addon('repository.kodiyhelp').getAddonInfo('version')
+
+except:
+    
