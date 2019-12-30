6ff6c4028e1614f0fca01e713fc3d2d13e044824
Goldengunrepo <47215262+Goldengunrepo@users.noreply.github.com>
2019-12-30 12:48:13 +0000

    Closed

--- goldengun/zips/script.block.tvaddons/script.block.tvaddons/startup.py
+++
@@ -1,5 +0,0 @@
-import xbmc
-
-xbmc.executeJSONRPC('{"jsonrpc": "2.0", "id":1, "method": "Addons.SetAddonEnabled", "params": { "addonid": "plugin.program.indigo", "enabled": false }}')
-xbmc.executeJSONRPC('{"jsonrpc": "2.0", "id":1, "method": "Addons.SetAddonEnabled", "params": { "addonid": "plugin.git.browser", "enabled": false }}')
-xbmc.executeJSONRPC('{"jsonrpc": "2.0", "id":1, "method": "Addons.SetAddonEnabled", "params": { "addonid": "script.tvaddons.debug.log", "enabled": false }}')
