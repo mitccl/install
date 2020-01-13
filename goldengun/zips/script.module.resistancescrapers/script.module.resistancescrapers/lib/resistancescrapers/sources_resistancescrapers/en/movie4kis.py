a83c9664e7ef3b675aee614f55ea09013264fd44
Goldengunrepo <47215262+Goldengunrepo@users.noreply.github.com>
2020-01-13 23:25:56 +0000

    Updates

--- goldengun/zips/script.module.resistancescrapers/script.module.resistancescrapers/lib/resistancescrapers/sources_resistancescrapers/en/movie4kis.py
+++ goldengun/zips/script.module.resistancescrapers/script.module.resistancescrapers/lib/resistancescrapers/sources_resistancescrapers/en/movie4kis.py
@@ -1,107 +1,87 @@
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
+# -Cleaned and Checked on 08-24-2019 by JewBMX in Scrubs.
+
+
 
 '''
-    
+    OpenScrapers Project
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import urllib, urlparse, re
+import re
+import urlparse
 
-from resources.lib.modules import cleantitle
-from resources.lib.modules import client
-from resources.lib.modules import source_utils
-from resources.lib.modules import dom_parser
-from resources.lib.modules import directstream
-from resources.lib.modules import debrid
-
+from resistancescrapers.modules import cfscrape
+from resistancescrapers.modules import cleantitle
+from resistancescrapers.modules import client
+from resistancescrapers.modules import jsunpack
+from resistancescrapers.modules import source_utils
 
 
 class source:
-    def __init__(self):
-        self.priority = 1
-        self.language = ['en']
-        self.domains = ['movie4k.is']
-        self.base_link = 'https://www1.movie4k.is/'
-        self.search_link = '/search/%s/feed/rss2/'
+	def __init__(self):
+		self.priority = 1
+		self.language = ['en']
+		self.domains = ['movie4k.is']  # Old  movie4k.ws
+		self.base_link = 'https://www2.movie4k.is'
+		self.search_link = '/search/%s/feed/rss2/'
+		self.scraper = cfscrape.create_scraper()
 
-    def movie(self, imdb, title, localtitle, aliases, year):
-        try:
-            url = {'imdb': imdb, 'title': title, 'year': year}
-            url = urllib.urlencode(url)
-            return url
-        except:
-            return
+	def movie(self, imdb, title, localtitle, aliases, year):
+		try:
+			search_id = cleantitle.getsearch(title)
+			url = urlparse.urljoin(self.base_link, self.search_link)
+			url = url % (search_id.replace(':', ' ').replace(' ', '+'))
+			h = {'User-Agent': client.randomagent()}
+			r = self.scraper.get(url, headers=h).content
+			z = re.compile('<item>(.+?)</item>', flags=re.DOTALL | re.UNICODE | re.MULTILINE | re.IGNORECASE).findall(r)
+			for t in z:
+				b = re.compile('<a rel="nofollow" href="(.+?)">(.+?)</a>',
+				               flags=re.DOTALL | re.UNICODE | re.MULTILINE | re.IGNORECASE).findall(t)
+				for foundURL, foundTITLE in b:
+					if cleantitle.get(title) in cleantitle.get(foundTITLE):
+						return foundURL
+			return
+		except:
+			return
 
-    def sources(self, url, hostDict, hostprDict):
-        try:
-            sources = []
+	def sources(self, url, hostDict, hostprDict):
+		try:
+			sources = []
+			if url is None:
+				return sources
+			h = {'User-Agent': client.randomagent()}
+			html = self.scraper.get(url, headers=h).content
+			qual = re.compile('<span class="calidad2">(.+?)</span>', flags=re.DOTALL | re.IGNORECASE).findall(html)[0]
+			links = re.compile('<iframe src="(.+?)"',
+			                   flags=re.DOTALL | re.UNICODE | re.MULTILINE | re.IGNORECASE).findall(html)
+			for link in links:
+				valid, host = source_utils.is_host_valid(link, hostDict)
+				quality, info = source_utils.get_release_quality(qual, link)
+				sources.append(
+					{'source': host, 'quality': quality, 'language': 'en', 'info': info, 'url': link, 'direct': False,
+					 'debridonly': False})
+			return sources
+		except:
+			return sources
 
-            if url == None: return sources
-
-            data = urlparse.parse_qs(url)
-            data = dict([(i, data[i][0]) if data[i] else (i, '') for i in data])
-            title = data['tvshowtitle'] if 'tvshowtitle' in data else data['title']
-
-            hdlr = 'S%02dE%02d' % (int(data['season']), int(data['episode'])) if 'tvshowtitle' in data else data['year']
-
-            query = '%s S%02dE%02d' % (data['tvshowtitle'], int(data['season']), int(data['episode'])) if 'tvshowtitle' in data else '%s' % (data['title'])
-            query = re.sub('(\\\|/| -|:|;|\*|\?|"|\'|<|>|\|)', ' ', query)
-
-            url = self.search_link % urllib.quote_plus(query)
-            url = urlparse.urljoin(self.base_link, url)
-
-            r = client.request(url)
-
-            posts = client.parseDOM(r, 'item')
-
-            items = []
-
-            for post in posts:
-
-                try:
-                    t = client.parseDOM(post, 'title')[0]
-                    t2 = re.sub('(\.|\(|\[|\s)(\d{4}|S\d*E\d*|S\d*|3D)(\.|\)|\]|\s|)(.+|)', '', t)
-
-                    if not cleantitle.get_simple(t2.replace('Watch Online','')) == cleantitle.get(title): raise Exception()
-
-                    l = client.parseDOM(post, 'link')[0]
- 
-                    p = client.parseDOM(post, 'pubDate')[0]
-
-                    if data['year'] in p: items += [(t, l)]
-
-                except:
-                    pass
-
-            print items
-            for item in items:
-                try:
-                    name = item[0]
-                    name = client.replaceHTMLCodes(name)
-                    
-                    u = client.request(item[1])
-                    if 'http://www.imdb.com/title/%s/' % data['imdb'] in u:
-                        
-                        l = client.parseDOM(u, 'div', {'class': 'movieplay'})[0]
-                        l = client.parseDOM(u, 'iframe', ret='data-lazy-src')[0]
-
-                        quality, info = source_utils.get_release_quality(name, l)
-                        info = ' | '.join(info)
-
-                        url = l
-
-                        url = client.replaceHTMLCodes(url)
-                        url = url.encode('utf-8')
-
-                        valid, host = source_utils.is_host_valid(url,hostDict)
-                        sources.append({'source': host, 'quality': quality, 'language': 'en', 'url': url,
-                                        'info': info, 'direct': False, 'debridonly': False})
-                except:
-                    pass
-
-            return sources
-        except:
-            return sources
-
-
-    def resolve(self, url):
-        return url
+	def resolve(self, url):
+		if 'streamty.com' in url:
+			h = {'User-Agent': client.randomagent()}
+			html = self.scraper.get(url, headers=h).content
+			packed = find_match(data, "text/javascript'>(eval.*?)\s*</script>")
+			unpacked = jsunpack.unpack(packed)
+			link = find_match(unpacked, 'file:"([^"]+)"')[0]
+			return link
+		return url
--- goldengun/zips/script.module.resistancescrapers/script.module.resistancescrapers/lib/resistancescrapers/sources_resistancescrapers/en/sharemovies.py
+++ goldengun/zips/script.module.resistancescrapers/script.module.resistancescrapers/lib/resistancescrapers/sources_resistancescrapers/en/sharemovies.py
@@ -1,13 +1,30 @@
+# -*- coding: UTF-8 -*-
+# -Cleaned and Checked on 05-06-2019 by JewBMX in Scrubs.
 
+
+
+'''
+    OpenScrapers Project
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 import base64
 import re
-import urllib
-import urlparse
-from resources.lib.modules import cleantitle
-from resources.lib.modules import client
-from resources.lib.modules import proxy
-from resources.lib.modules import cfscrape
+
+from resistancescrapers.modules import cfscrape
+from resistancescrapers.modules import cleantitle
+from resistancescrapers.modules import source_utils
 
 
 class source:
@@ -17,38 +34,39 @@ class source:
 		self.domains = ['sharemovies.net']
 		self.base_link = 'http://sharemovies.net'
 		self.search_link = '/search-movies/%s.html'
+		self.scraper = cfscrape.create_scraper()
 
 	def movie(self, imdb, title, localtitle, aliases, year):
 		try:
 			q = cleantitle.geturl(title)
-			q2 = q.replace('-','+')
+			q2 = q.replace('-', '+')
 			url = self.base_link + self.search_link % q2
-			r = client.request(url)
-			match = re.compile('<div class="title"><a href="(.+?)">'+title+'</a></div>').findall(r)
+			r = self.scraper.get(url).content
+			match = re.compile('<div class="title"><a href="(.+?)">' + title + '</a></div>').findall(r)
 			for url in match:
 				return url
 		except:
 			return
-			
+
 	def tvshow(self, imdb, tvdb, tvshowtitle, localtvshowtitle, aliases, year):
 		try:
 			url = cleantitle.geturl(tvshowtitle)
 			return url
 		except:
 			return
- 
+
 	def episode(self, url, imdb, tvdb, title, premiered, season, episode):
 		try:
-			if not url: return
-			
+			if url is None:
+				return sources
 			q = url + '-season-' + season
-			q2 = url.replace('-','+')
+			q2 = url.replace('-', '+')
 			url = self.base_link + self.search_link % q2
-			r = client.request(url)
-			match = re.compile('<div class="title"><a href="(.+?)-'+q+'\.html"').findall(r)
+			r = self.scraper.get(url).content
+			match = re.compile('<div class="title"><a href="(.+?)-' + q + '\.html"').findall(r)
 			for url in match:
-				url = '%s-%s.html' % (url,q)
-				r = client.request(url)
+				url = '%s-%s.html' % (url, q)
+				r = self.scraper.get(url).content
 				match = re.compile('<a class="episode episode_series_link" href="(.+?)">' + episode + '</a>').findall(r)
 				for url in match:
 					return url
@@ -57,22 +75,22 @@ class source:
 
 	def sources(self, url, hostDict, hostprDict):
 		try:
+			if url is None:
+				return sources
 			sources = []
-			r = client.request(url)
+			r = self.scraper.get(url).content
 			try:
-				match = re.compile('themes/movies/img/icon/server/(.+?)\.png" width="16" height="16" /> <a href="(.+?)">Version ').findall(r)
+				match = re.compile(
+					'themes/movies/img/icon/server/(.+?)\.png" width="16" height="16" /> <a href="(.+?)">Version ').findall(
+					r)
 				for host, url in match:
-					if host == 'internet':
-						pass
-					else:
-						sources.append({
-							'source': host,
-							'quality': 'SD',
-							'language': 'en',
-							'url': url,
-							'direct': False,
-							'debridonly': False
-						})
+					if host == 'internet': pass
+					if host in str(sources): continue
+					if url in str(sources): continue
+					valid, host = source_utils.is_host_valid(host, hostDict)
+					if valid:
+						sources.append({'source': host, 'quality': 'SD', 'language': 'en', 'url': url, 'direct': False,
+						                'debridonly': False})
 			except:
 				return
 		except Exception:
@@ -80,7 +98,7 @@ class source:
 		return sources
 
 	def resolve(self, url):
-		r = client.request(url)
+		r = self.scraper.get(url).content
 		match = re.compile('Base64\.decode\("(.+?)"').findall(r)
 		for iframe in match:
 			iframe = base64.b64decode(iframe)
